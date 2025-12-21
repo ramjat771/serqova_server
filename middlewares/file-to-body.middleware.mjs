574ef9d6bd24308969e74aa3c1bc84bca6fe3cee
sunita0070 <sunitaq26@gmail.com>
2025-12-21 22:32:03 +0530

    multer change

--- middlewares/file-to-body.middleware.mjs
+++ middlewares/file-to-body.middleware.mjs
@@ -1,19 +1,43 @@
-export const filesToBody = (req, res, next) => {
-   
-  if (req.files && req.files.length) {
-    req.files.forEach((file) => {
-      const fieldName = file.fieldname;
-      const fileUrl = file.path; 
-      if (req.body[fieldName]) {
-        if (Array.isArray(req.body[fieldName])) {
-          req.body[fieldName].push(fileUrl);
+import { v2 as cloudinary } from "cloudinary";
+
+export const filesToBody = async (req, res, next) => {
+  try {
+    if (req.files && req.files.length > 0) {
+      const uploadPromises = req.files.map((file) => {
+        return new Promise((resolve, reject) => {
+          const stream = cloudinary.uploader.upload_stream(
+            {
+              folder: "jhamkoo_uploads",
+              public_id:
+                file.originalname.split(".")[0] + "_" + Date.now(),
+            },
+            (err, result) => {
+              if (err) return reject(err);
+              resolve({ field: file.fieldname, url: result.secure_url });
+            }
+          );
+
+          stream.end(file.buffer);
+        });
+      });
+
+      const uploadedFiles = await Promise.all(uploadPromises);
+
+      uploadedFiles.forEach(({ field, url }) => {
+        if (req.body[field]) {
+          if (Array.isArray(req.body[field])) {
+            req.body[field].push(url);
+          } else {
+            req.body[field] = [req.body[field], url];
+          }
         } else {
-          req.body[fieldName] = [req.body[fieldName], fileUrl];
+          req.body[field] = url;
         }
-      } else {
-        req.body[fieldName] = fileUrl;
-      }
-    });
+      });
+    }
+
+    next();
+  } catch (error) {
+    next(error);
   }
-  next();
 };
--- middlewares/multer.mjs
+++ middlewares/multer.mjs
@@ -1,17 +1,11 @@
 import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
+
 cloudinary.config({
   cloud_name: "dxladutih",
   api_key: "989343698562726",
   api_secret: "BnvO_BLn07A_5m2WHohFyvoaPlg",
 });
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-         public_id: (req, file)=> { 
-      const name = file.originalname.split(".")[0];
-      return `${name}_${Date.now()}`},             // unique name
-  },
+export const upload = multer({
+  storage: multer.memoryStorage(),
 });
-export const upload = multer({ storage });
