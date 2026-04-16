3c96b651cc1ee26a7402671acec3f59e45b56a67
sunita0070 <sunitaq26@gmail.com>
2026-04-16 22:26:03 +0530

    change multer

--- middlewares/file-to-body.middleware.mjs
+++ middlewares/file-to-body.middleware.mjs
@@ -1,35 +1,13 @@
-export const filesToBody = async (req, res, next) => {
-  try {
-    const allFiles = Object.values(req.files || {}).flat();
+export const filesToBody = (req, res, next) => {
+  if (req.files) {
+    Object.keys(req.files).forEach((key) => {
+      const file = req.files[key]?.[0];
 
-    if (allFiles.length > 0) {
-      const uploadPromises = allFiles.map((file) => {
-        return new Promise((resolve, reject) => {
-          const stream = cloudinary.uploader.upload_stream(
-            {
-              folder: "jhamkoo_uploads",
-              public_id:
-                file.originalname.split(".")[0] + "_" + Date.now(),
-            },
-            (err, result) => {
-              if (err) return reject(err);
-              resolve({ field: file.fieldname, url: result.secure_url });
-            }
-          );
-
-          stream.end(file.buffer);
-        });
-      });
-
-      const uploadedFiles = await Promise.all(uploadPromises);
-
-      uploadedFiles.forEach(({ field, url }) => {
-        req.body[field] = url;
-      });
-    }
-
-    next();
-  } catch (error) {
-    next(error);
+      if (file) {
+        req.body[key] = file.path; // ✅ Cloudinary URL
+      }
+    });
   }
+
+  next();
 };
--- middlewares/multer.mjs
+++ middlewares/multer.mjs
@@ -1,11 +1,18 @@
+
 import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
-
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 cloudinary.config({
-  cloud_name: "dnervrywy",
+cloud_name: "dnervrywy",
   api_key: "972722711284976",
   api_secret: "vHTX_s8vVrXa_rQn-t7A4fwWiWA",
 });
-export const upload = multer({
-  storage: multer.memoryStorage(),
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+         public_id: (req, file)=> { 
+      const name = file.originalname.split(".")[0];
+      return `${name}_${Date.now()}`},             // unique name
+  },
 });
+export const upload = multer({ storage });
