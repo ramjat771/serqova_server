0327622d11dabaebc3518a6d9935bb7213fd5c32
sunita0070 <sunitaq26@gmail.com>
2026-04-16 21:34:25 +0530

    add user

--- routes/user.routes.mjs
+++ routes/user.routes.mjs
@@ -4,5 +4,7 @@ import * as userController from "../controller/user.controller.mjs";
 const router = express.Router();
 
 router.post("/login", userController.loginUserController);
-
+router.get("/",(req, res)=>{
+    res.send("User route is working fine");
+});
 export default router;
